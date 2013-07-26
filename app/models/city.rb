d547f2aef6b3fb25438eb8cd3b56068d5985a62f
MpaK <mrak69@gmail.com>
2013-07-26 14:58:25 +0700

    Check City.alias to uniqueness, Seed file for Cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,4 @@
 class City < ActiveRecord::Base
   validates_presence_of :alias, :name
-  validates :alias, length: { maximum:32, tokenizer:lambda{ |str| str.scan(/\w+/)} }
+  validates :alias, length: { maximum:32, tokenizer:lambda{ |str| str.scan(/\w+/)} }, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# eNNNcoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+City.destroy_all
+cities = City.create([{ alias:'ufa', name:'Уфа' }, { alias:'kazan', name:'Казань' }, { alias:'chelny', name:'Набережные Челны' }])
