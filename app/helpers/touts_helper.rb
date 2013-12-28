e03a59de021e97a915b20e0062f95caaffb01c9e
MpaK <mrak69@gmail.com>
2013-12-28 15:34:00 +0700

    Helper for Checking owner authorization

--- app/helpers/touts_helper.rb
+++ app/helpers/touts_helper.rb
@@ -1,2 +1,8 @@
 module ToutsHelper
+  ##
+  # Show block only for user with admin role
+  def only_for_owner(toute, &block)
+    yield block if user_signed_in? && (current_user.admin? || current_user.id == toute.user_id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_many :touts
+
+  ##
+  # Is current_user admin?
+  def admin?
+    role == 'admin'
+  end
 end
